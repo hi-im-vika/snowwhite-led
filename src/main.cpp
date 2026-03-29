305c0c93fc39c4da09617f0750caca501247b4d5
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-28 19:06:21 -0700

    Remove placeholder code

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,8 @@
 #define SENSE_PIN 15      // sense pin to detect if strip is connected
 #define PATT_PIN 2        // button pin to switch animations
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
