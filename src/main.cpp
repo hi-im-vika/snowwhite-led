ec876cdbf4bb91f3bcb3dccc990c2fdfafc619e1
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-28 19:51:16 -0700

    Add defines for primary hue and limits for wave functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #define SENSE_PIN 7       // sense pin to detect if strip is connected
 #define PATT_PIN 2        // button pin to switch animations
 #define LED_COUNT 32      // how many LEDs on strip
+#define PRIMARY_HUE 80
+#define WAVE_MAX 255
+#define WAVE_MIN 32
 
 CRGBArray<LED_COUNT> strip;
 
