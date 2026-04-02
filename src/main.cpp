c7fc5afff7ad470fc1019cc67a4102d755dfcba3
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-04-02 05:26:08 -0700

    Change pin defines

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define LED_PIN 6         // data pin for LED strip
-#define SENSE_PIN 7       // sense pin to detect if strip is connected
+#define LED_PIN 10         // data pin for LED strip
+#define SENSE_PIN 16       // sense pin to detect if strip is connected
 #define PATT_PIN 2        // button pin to switch animations
 #define LED_COUNT 32      // how many LEDs on strip
 #define PRIMARY_HUE 24
